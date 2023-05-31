85443fad5a564d2958d222abb402c06592423e38
SerHappy <lokhmanovkirill@gmail.com>
2023-05-31 21:13:15 +0300

    Deleted deprecated folders

--- deprecated/db/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .media import Media
-from .announcement import Announcement
-from .tag import Tag
-from .user import User
-
-__all__ = ["Media", "Announcement", "Tag", "User"]
--- deprecated/db/models/tag.py
+++
@@ -1,7 +0,0 @@
-from tortoise import fields
-from tortoise.models import Model
-
-
-class Tag(Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=50, unique=True)
