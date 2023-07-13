2306e5bdf8865565e42386d5fd5de0c24ce4461b
SerHappy <lokhmanovkirill@gmail.com>
2023-07-13 20:14:33 +0300

    Bug fix

--- web/static/js/delete_tag.js
+++ web/static/js/delete_tag.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
     $(document).on('click', '#deleteModalButton', function () {
-        let deleteUrl = $(this).data('delete-url');
-        let redirectUrl = $(this).data('redirect-url');
-
         $("#deleteModal").modal('show');
 
         $("#deleteButton").click(function () {
-            let cookie = document.cookie
-            let csrfToken = cookie.substring(cookie.indexOf('=') + 1)
+            let deleteUrl = $(this).data('delete-url');
+            let redirectUrl = $(this).data('redirect-url');
+            let cookie = document.cookie;
+            let csrfToken = cookie.substring(cookie.indexOf('=') + 1);
             $.ajax({
                 url: deleteUrl,
                 type: 'POST',
