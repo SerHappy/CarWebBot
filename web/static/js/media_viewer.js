36651e9afb0b17db86d727912631226a9deda90b
SerHappy <lokhmanovkirill@gmail.com>
2023-07-14 20:45:47 +0300

    Fixed Media Modal

--- web/static/js/media_viewer.js
+++ web/static/js/media_viewer.js
@@ -1,10 +1,9 @@
-
 $(document).ready(function () {
     var currentMediaIndex = -1;
     var currentRowId = null;
     var mediaMap = {};
 
-    window.updateMediaMap = function () {
+    function updateMediaMap() {
         mediaMap = {};
         $(".media-links").each(function () {
             var rowId = $(this).data("announcement-id");
@@ -15,48 +14,66 @@ $(document).ready(function () {
         });
     }
 
-    $(".media-links").each(function () {
-        var rowId = $(this).data("announcement-id");
-        if (!(rowId in mediaMap)) {
-            mediaMap[rowId] = [];
-        }
-        mediaMap[rowId].push($(this).data("src"));
-    });
-
-    $("#mediaModal").on("show.bs.modal", function (event) {
-        var button = $(event.relatedTarget);
-        currentRowId = button.data("announcement-id");
-        currentMediaIndex = button.data("media-index");
-        var src = button.data("src");
-        loadMedia(src);
-    });
+    updateMediaMap();
 
     $("#previous").on("click", function () {
-        if (currentMediaIndex > 0) {
-            currentMediaIndex--;
-            loadMedia(mediaMap[currentRowId][currentMediaIndex]);
-        }
-    });
-
-    $("#next").on("click", function () {
-        console.log(currentMediaIndex);
-        console.log(mediaMap);
-        if (currentMediaIndex < mediaMap[currentRowId].length - 1) {
-            currentMediaIndex++;
-            loadMedia(mediaMap[currentRowId][currentMediaIndex]);
-        }
-    });
-
-    function loadMedia(src) {
-        var mediaHtml;
-        if (src.endsWith(".mp4") || src.endsWith(".mov")) {
-            mediaHtml =
-                '<video controls><source src="' +
-                src +
-                '" type="video/mp4">Your browser does not support the video tag.</video>';
-        } else {
-            mediaHtml = '<img src="' + src + '" class="img-fluid">';
-        }
-        $("#mediaModal").find(".modal-body").html(mediaHtml);
+    if (currentMediaIndex > 0) {
+        currentMediaIndex--;
+        loadMedia(mediaMap[currentRowId][currentMediaIndex]);
     }
+    checkButtonStatus();
+});
+
+$("#next").on("click", function () {
+    if (currentMediaIndex < mediaMap[currentRowId].length - 1) {
+        currentMediaIndex++;
+        loadMedia(mediaMap[currentRowId][currentMediaIndex]);
+    }
+    checkButtonStatus();
+});
+
+function loadMedia(src) {
+    var mediaHtml;
+    if (src.endsWith(".mp4") || src.endsWith(".mov")) {
+        mediaHtml =
+            '<video controls><source src="' +
+            src +
+            '" type="video/mp4">Your browser does not support the video tag.</video>';
+    } else {
+        mediaHtml = '<img src="' + src + '" class="img-fluid">';
+    }
+    $("#mediaModal").find(".modal-body").html(mediaHtml);
+    updateMediaCounter();
+}
+
+function updateMediaCounter() {
+    $('#media-counter').text((currentMediaIndex + 1) + '/' + mediaMap[currentRowId].length);
+}
+
+function checkButtonStatus() {
+    $('#previous').prop('disabled', currentMediaIndex === 0);
+    $('#next').prop('disabled', currentMediaIndex === mediaMap[currentRowId].length - 1);
+}
+
+$("#mediaModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget);
+    currentRowId = button.data("announcement-id");
+    updateMediaMap();
+    var src = button.data("src");
+    currentMediaIndex = mediaMap[currentRowId].indexOf(src);
+    loadMedia(src);
+    checkButtonStatus();
+});
+});
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left arrow key
+            $("#previous").trigger('click');
+            break;
+        case 39: // right arrow key
+            $("#next").trigger('click');
+            break;
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
 });
