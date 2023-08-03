5aacde0950e93f02187a48de22322eb31eb4860c
SerHappy <lokhmanovkirill@gmail.com>
2023-08-04 02:38:03 +0300

    Added telethon service to bot app

---
+++ web/apps/bot/services/telethon.py
@@ -0,0 +1,53 @@
+from decouple import config
+from loguru import logger
+from telethon.sync import TelegramClient
+from typing import Any
+
+import asyncio
+import concurrent.futures
+
+
+def fetch_telegram_client() -> TelegramClient:
+    """Возвращает клиента Telegram, заданного в env файле."""
+    return TelegramClient(
+        session=config("TELETHON_SESSION_NAME"),
+        api_id=config("TELETHON_API_ID"),
+        api_hash=config("TELETHON_API_HASH"),
+        system_version=config("TELETHON_SYSTEM_VERSION"),
+    )
+
+
+def run_in_new_thread(func, *args, **kwargs) -> Any:
+    """Запускает функцию `func` в отдельном потоке."""
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(func, *args, **kwargs)
+        concurrent.futures.wait([future])
+        _check_future_status(future)
+        return future.result()
+
+
+def _check_future_status(future: concurrent.futures.Future) -> None:
+    """Проверяет статус задачи `future`, запущенной в отдельном потоке."""
+    if future.cancelled():
+        logger.error("Task was cancelled")
+        return
+
+    if future.done():
+        try:
+            future.result()
+        except Exception as e:
+            logger.error(f"Task ended with an error: {e}")
+            return
+        else:
+            logger.info("Task ended successfully")
+            return
+
+    if future.running():
+        logger.info("Task is running")
+        return
+
+
+def set_new_event_loop() -> None:
+    """Создает новый event loop для asyncio."""
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
