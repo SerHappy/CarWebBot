f44385d267c7d0dcfc5a0bcdaa0d3fe7cf15feae
SerHappy <lokhmanovkirill@gmail.com>
2023-08-05 16:14:38 +0300

    Fixed loguru and added order by cat. in ann. list

--- web/apps/announcement/views/list.py
+++ web/apps/announcement/views/list.py
@@ -4,6 +4,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 from django.core.paginator import Paginator
+from django.db.models import Case
+from django.db.models import IntegerField
+from django.db.models import Value
+from django.db.models import When
 from django.views.generic import ListView
 from loguru import logger
 from typing import Any
@@ -16,7 +20,15 @@ class AnnouncementListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["announcements"] = Announcement.objects.all()
+        context["announcements"] = Announcement.objects.annotate(
+            custom_order=Case(
+                When(is_active=True, is_published=False, processing_status="P", then=Value(1)),
+                When(is_active=True, is_published=True, processing_status="D", then=Value(2)),
+                When(is_active=False, is_published=False, processing_status="P", then=Value(3)),
+                default=Value(4),
+                output_field=IntegerField(),
+            )
+        ).order_by("custom_order", "-publication_date", "-id")
         name_filter = self.request.GET.get("nameFilter")
         tag_filter = self.request.GET.get("tagFilter")
         if name_filter:
