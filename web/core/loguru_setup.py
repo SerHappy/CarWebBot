a7adb208e78db4b12b2e187de11d4ff4a5184f73
SerHappy <lokhmanovkirill@gmail.com>
2023-08-05 16:19:45 +0300

    Added loguru all and warnings folders

--- web/core/loguru_setup.py
+++ web/core/loguru_setup.py
@@ -1,20 +1,31 @@
 from django.conf import settings
 
+import os
+
 
 def setup_logger(logger, settings_dict=None) -> None:
     """
     Setup logger with settings from .env file
 
-    All log files are stored in LOGURU_PATH directory.
+    All log files are stored in LOGURU_FOLDER directory.
     Every day at midnight log files are rotated and compressed.
     """
     logger.remove()
     logger.add(
-        settings.LOGURU_PATH,
+        os.path.join(settings.LOGURU_FOLDER, "all/server.log"),
         rotation="0:00",
         enqueue=True,
         compression="zip",
         level=settings.LOGURU_LEVEL,
+        format=settings.LOGURU_FORMAT,
+        serialize=False,
+    )
+    logger.add(
+        os.path.join(settings.LOGURU_FOLDER, "warnings/server.log"),
+        rotation="0:00",
+        enqueue=True,
+        compression="zip",
+        level="WARNING",
         format=settings.LOGURU_FORMAT,
         serialize=False,
     )
