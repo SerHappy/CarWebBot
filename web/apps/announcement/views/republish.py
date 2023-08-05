24fce89e8c1e4df144e3026d675c95536c93d477
SerHappy <lokhmanovkirill@gmail.com>
2023-08-05 16:31:17 +0300

    Fixed republish

--- web/apps/announcement/views/republish.py
+++ web/apps/announcement/views/republish.py
@@ -24,19 +24,21 @@ def republish_announcement(request: HttpRequest, pk: int) -> HttpResponse:
     if announcement.is_published:
         delete_announcement_from_channel(announcement)
 
+    new_date = request.POST.get("datetime")
+    timezone = request.POST.get("timezone")
+    pytz_timezone = pytz.timezone(timezone)
+    date_format = "%d.%m.%Y %H:%M"
+    date_without_tz = datetime.strptime(new_date, date_format)
+    date_with_tz = pytz_timezone.localize(date_without_tz)
+    if date_with_tz < datetime.now(pytz_timezone):
+        now = datetime.now(pytz_timezone)
+        date_with_tz = now.replace(second=0, microsecond=0) + timedelta(minutes=1)
+    date_utc = date_with_tz.astimezone(pytz.UTC)
+    announcement.publication_date = date_utc
+
     keys = request.POST.keys()
     if set(keys) == {"datetime", "timezone"}:
-        new_date = request.POST.get("datetime")
-        timezone = request.POST.get("timezone")
-        pytz_timezone = pytz.timezone(timezone)
-        date_format = "%d.%m.%Y %H:%M"
-        date_without_tz = datetime.strptime(new_date, date_format)
-        date_with_tz = pytz_timezone.localize(date_without_tz)
-        if date_with_tz < datetime.now(pytz_timezone):
-            now = datetime.now(pytz_timezone)
-            date_with_tz = now.replace(second=0, microsecond=0) + timedelta(minutes=1)
-        date_utc = date_with_tz.astimezone(pytz.UTC)
-        announcement.publication_date = date_utc
+        pass
     else:
         announcement, tag_ids = form_data_handler.handle_form_data(request, announcement)
         tags_handler.handle_tags(tag_ids, announcement)
