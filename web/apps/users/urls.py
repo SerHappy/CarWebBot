88478fb12b1bd45b202c46a85967de136903dc51
SerHappy <lokhmanovkirill@gmail.com>
2023-08-07 17:43:18 +0300

    Updated README

--- web/apps/users/urls.py
+++ web/apps/users/urls.py
@@ -1,11 +1,13 @@
 from .views import LoginView
 from .views import LogoutView
 from .views import RegisterView
+from django.conf import settings
 from django.urls import path
 
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
+if settings.DEBUG:
+    urlpatterns += path("register/", RegisterView.as_view(), name="register")
