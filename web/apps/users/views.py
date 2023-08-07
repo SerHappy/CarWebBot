3886a96072f7758e829e8d46b0e2ec3bc6f2ab40
SerHappy <lokhmanovkirill@gmail.com>
2023-08-07 17:47:36 +0300

    Fix login page

--- web/apps/users/views.py
+++ web/apps/users/views.py
@@ -5,10 +5,11 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.base import RedirectView
 from django.views.generic.edit import FormView
-from django.shortcuts import redirect
+
 
 class RegisterView(FormView):
     template_name = "register/register.html"
@@ -26,6 +27,12 @@ class LoginView(FormView):
     template_name = "login/login.html"
     form_class = UserLoginForm
     success_url = reverse_lazy("announcement-list")
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect("announcement-list")
+        else:
+            return super().get(request, *args, **kwargs)
 
     def form_valid(self, form) -> HttpResponse | None:
         user = authenticate(self.request, username=form.cleaned_data["email"], password=form.cleaned_data["password"])
